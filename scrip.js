ffbe2fa7e11c2c80689616382dc9310a950cee36
Bryan <bryan@MacBook-Air.local>
2024-02-20 22:26:24 -0500

    added funtionality to button and nav links using javascript.

--- scrip.js
+++ scrip.js
@@ -5,3 +5,24 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
     menuToggle.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  }
